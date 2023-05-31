d7295af61fa1260c1bdac6b98d0c38c61499d114
Yousaf-R878 <127049010+Yousaf-R878@users.noreply.github.com>
2023-05-31 11:11:33 -0400

    Add files via upload

---
+++ Runner Game Scripts/Win.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Overview: 
+//this script activates when the player beats a level
+public class Win : MonoBehaviour
+{
+
+    //This will destroy the player if it reaches the end of the level and will output "You've beaten the lvl"
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("You've beaten the lvl");
+        }
+    }
+}
