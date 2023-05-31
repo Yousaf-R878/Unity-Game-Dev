1d99dd312b2189b3e68da8f7bae0b13797f627ef
Yousaf-R878 <127049010+Yousaf-R878@users.noreply.github.com>
2023-05-31 11:25:07 -0400

    Add files via upload

---
+++ Platformer Scripts/ToDO.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDO : MonoBehaviour
+{
+    //this is a to-do list
+
+
+    //Boss:
+    //lots of health | X
+    //shoot at player | X
+    //have a bomb move that will essentially use the same principle as melee combat with the range
+    //effects?
+    //spawn manager that spawns a boss every 10 waves and spawns enemies on a timed interval
+    //^^ possibly stop normal enemies from spawning when boss spawns
+    //implement health pack spawning randomly but only spawns after a previous one has been picked up
+    //damage power up?
+    //buff melee combat?
+
+
+    // Done Yesterday:
+    // implement melee combat for player
+    // implement melee combat for enemy 
+    // implement boss
+    // add health pack
+    // started enemy spawner
+    //(10/1/20
+
+
+
+
+    //ALMOST 700 LINES OF CODE 
+    //AROUND 690 LINES
+}
