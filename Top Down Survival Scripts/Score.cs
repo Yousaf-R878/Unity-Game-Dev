11b21eb503e0c2133ec68f07b2fbe0ffa0545bcb
Yousaf-R878 <127049010+Yousaf-R878@users.noreply.github.com>
2023-05-31 11:15:34 -0400

    Add files via upload

---
+++ Top Down Survival Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour
+{
+
+    public Text scoreText;
+    public PlayerController playerCont;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        playerCont = player.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //scoreText.text = playerCont.score.ToString();
+        //Debug.Log(playerCont.score);
+    }
+}
